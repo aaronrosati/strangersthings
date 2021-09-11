d7ade7afe7cdfe7acf50d6428a0633fbb89802f8
aaronrosati <84755343+aaronrosati@users.noreply.github.com>
2021-09-11 11:48:48 -0700

    v13

--- src/components/FilterPosts.js
+++ src/components/FilterPosts.js
@@ -1,37 +1,43 @@
-import React, { useState, useEffect} from 'react';
-import FetchSinglePost from './FetchSinglePost';
+import React, {useState} from 'react';
 
+const FilterPosts = (props) => {
+    const {setPosts, posts, fetchPosts} = props;
+    const [searchTitle, setSearchTitle] = useState('');
+    const [searchDescription, setSearchDescription] = useState('');
+    let filteredPosts =[];
 
-function FilterPosts({posts, setPosts}) {
-    const [searchTerm, setSearchTerm] = useState('');
-    const postMatches = (post, text) => {
-        let check = post.includes(text);
-        return check;
-    }
-    const handleSubmit = () => {
-        const filteredPosts = posts.filter(post => postMatches(post.title.toLowerCase(), searchTerm));
+    const filterPostsTitle = async () => {
+        filteredPosts = posts.filter(post => post.title.toLowerCase().includes(searchTitle.toLowerCase()));
         setPosts(filteredPosts);
-        if (!searchTerm.length) {
-            fetchPosts();
-        }
     }
-    const fetchPosts = async () => {
-        const response = await callApi({
-            url: '/posts',
-});
-const allPosts = response.data.posts;
-        if(allPosts) setPosts(allPosts);
-    }
-    
-return <>
-<form onSubmit={(ev) => {
-    ev.preventDefault()
-    handleSubmit()
-}}>
-    <input type="text" placeholder="search..." value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}></input>
-    <button type="submit">Search</button>
-</form>
 
-</>
+    const filterPostsDescription = async () => {
+        filteredPosts = posts.filter(post => post.description.toLowerCase().includes(searchDescription.toLowerCase()));
+        setPosts(filteredPosts);
+    }
+
+    return <div>
+            <form onChange={filterPostsTitle} onSubmit={async(event) => {
+                event.preventDefault();
+                await fetchPosts();
+                setSearchTitle('');
+            }}>
+                <label>Search By Title </label>
+                <input type='text' name='search-term' placeholder='search term' value={searchTitle}
+                onChange={(event) => setSearchTitle(event.target.value)}></input>
+                <button type='submit'>Reset Search Results</button>
+            </form>
+            <form onChange={filterPostsDescription} onSubmit={async(event) => {
+                event.preventDefault();
+                await fetchPosts();
+                setSearchDescription('');
+            }}>
+                <label>Search By Description </label>
+                <input type='text' name='search-term' placeholder='search term' value={searchDescription}
+                onChange={(event) => setSearchDescription(event.target.value)}></input>
+                <button type='submit'>Reset Search Results</button>
+            </form>
+        </div>
 }
+
 export default FilterPosts;
